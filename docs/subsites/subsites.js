a3a57fa851f5e3ee3ff4e30a4d2e6fb213f07689
Severin Gusenleitner <Severin.gusenleitner@gmail.com>
2026-01-13 10:14:29 +0100

    made the createBar function(has parameters for the width and stuff so i dont have to write as much code

--- docs/subsites/subsites.js
+++ docs/subsites/subsites.js
@@ -1,96 +1,28 @@
 const svg = document.getElementById("airlines-chart");
 
+function createBar({ y, width, label }) {
+  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  rect.setAttribute("x", "0");
+  rect.setAttribute("y", y);
+  rect.setAttribute("width", width);
+  rect.setAttribute("height", "50");
+  rect.setAttribute("class", "bar");
+  svg.appendChild(rect);
 
-createAirlinesCost();
-createAirlinesSpeed();
+  const tooltip = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  tooltip.setAttribute("fill", "white");
+  tooltip.setAttribute("font-size", "16");
+  tooltip.setAttribute("visibility", "hidden");
+  tooltip.textContent = label;
+  svg.appendChild(tooltip);
 
-function createAirlinesCost(){
-    const cost = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    cost.setAttribute("y", "25");
-    cost.setAttribute("x", "0");
-    cost.setAttribute("width", "250");
-    cost.setAttribute("height", "50");
-    cost.setAttribute("class", "bar");
-    
-    svg.appendChild(cost);
-    
-    const tooltip = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    
-    tooltip.setAttribute("fill", "white");
-    tooltip.setAttribute("font-size", "16");
+  rect.addEventListener("mouseenter", () => {
+    tooltip.setAttribute("x", 5);
+    tooltip.setAttribute("y", Number(y) + 30);
+    tooltip.setAttribute("visibility", "visible");
+  });
+
+  rect.addEventListener("mouseleave", () => {
     tooltip.setAttribute("visibility", "hidden");
-    tooltip.textContent = "Cost";
-    svg.appendChild(tooltip);
-    
-    // Show tooltip on hover
-    cost.addEventListener("mouseenter", () => {
-      tooltip.setAttribute("x", parseFloat(cost.getAttribute("x"))+5);
-      tooltip.setAttribute("y", parseFloat(cost.getAttribute("y")) + 30);
-      tooltip.setAttribute("visibility", "visible");
-    });
-    
-    // Hide tooltip when mouse leaves
-    cost.addEventListener("mouseleave", () => {
-      tooltip.setAttribute("visibility", "hidden");
-    });
-}
-function createAirlinesSpeed(){
-    const speed = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    speed.setAttribute("y", "100");
-    speed.setAttribute("x", "0");
-    speed.setAttribute("width", "380");
-    speed.setAttribute("height", "50");
-    speed.setAttribute("class", "bar");
-    
-    svg.appendChild(speed);
-    
-    const tooltip = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    
-    tooltip.setAttribute("fill", "white");
-    tooltip.setAttribute("font-size", "16");
-    tooltip.setAttribute("visibility", "hidden");
-    tooltip.textContent = "Speed";
-    svg.appendChild(tooltip);
-    
-    // Show tooltip on hover
-    speed.addEventListener("mouseenter", () => {
-      tooltip.setAttribute("x", parseFloat(speed.getAttribute("x"))+5);
-      tooltip.setAttribute("y", parseFloat(speed.getAttribute("y")) + 30);
-      tooltip.setAttribute("visibility", "visible");
-    });
-    
-    // Hide tooltip when mouse leaves
-    speed.addEventListener("mouseleave", () => {
-      tooltip.setAttribute("visibility", "hidden");
-    });
-}
-function createAirlinesSpeed(){
-    const speed = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    speed.setAttribute("y", "100");
-    speed.setAttribute("x", "0");
-    speed.setAttribute("width", "380");
-    speed.setAttribute("height", "50");
-    speed.setAttribute("class", "bar");
-    
-    svg.appendChild(speed);
-    
-    const tooltip = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    
-    tooltip.setAttribute("fill", "white");
-    tooltip.setAttribute("font-size", "16");
-    tooltip.setAttribute("visibility", "hidden");
-    tooltip.textContent = "Speed";
-    svg.appendChild(tooltip);
-    
-    // Show tooltip on hover
-    speed.addEventListener("mouseenter", () => {
-      tooltip.setAttribute("x", parseFloat(speed.getAttribute("x"))+5);
-      tooltip.setAttribute("y", parseFloat(speed.getAttribute("y")) + 30);
-      tooltip.setAttribute("visibility", "visible");
-    });
-    
-    // Hide tooltip when mouse leaves
-    speed.addEventListener("mouseleave", () => {
-      tooltip.setAttribute("visibility", "hidden");
-    });
+  });
 }
