dc14e7cf0cb365d008c71faea72d67e688231f06
Edi Veljacic <e.veljacic@students.htl-leonding.ac.at>
2026-01-14 08:27:47 +0100

    renamed the svg to airlineChart

--- docs/javascript/subsites.js
+++ docs/javascript/subsites.js
@@ -1,25 +1,26 @@
-const svg = document.getElementById("airlines-chart");
-createLine();
-function createLine(){
-    const line = document.createElementNS("http://www.w3.org/2000/svg","line");
-    line.setAttribute("x1", "100");
-    line.setAttribute("y1", "310");
-    line.setAttribute("x2","500");
-    line.setAttribute("y2", "310");
-    line.setAttribute("stroke","black");
-    line.setAttribute("stroke-width","2");
-    svg.appendChild(line);
+const airlineChart = document.getElementById("airlines-chart");
 
-    for(let i = 0; i<4; i++){
-      const line = document.createElementNS("http://www.w3.org/2000/svg","line");
-      line.setAttribute("x1", `${i*133+100}`);
-      line.setAttribute("x2", `${i*133+100}`);
-      line.setAttribute("y1", `305`);
-      line.setAttribute("y2", `315`);
-      line.setAttribute("stroke","black");
-      line.setAttribute("stroke-width","2");
-      svg.appendChild(line);
-    }
+createLine();
+function createLine() {
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line.setAttribute("x1", "100");
+  line.setAttribute("y1", "310");
+  line.setAttribute("x2", "500");
+  line.setAttribute("y2", "310");
+  line.setAttribute("stroke", "black");
+  line.setAttribute("stroke-width", "2");
+  airlineChart.appendChild(line);
+
+  for (let i = 0; i < 4; i++) {
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", `${i * 133 + 100}`);
+    line.setAttribute("x2", `${i * 133 + 100}`);
+    line.setAttribute("y1", `305`);
+    line.setAttribute("y2", `315`);
+    line.setAttribute("stroke", "black");
+    line.setAttribute("stroke-width", "2");
+    airlineChart.appendChild(line);
+  }
 }
 
 function createBar({ y, width, label }) {
@@ -29,24 +30,25 @@ function createBar({ y, width, label }) {
   rect.setAttribute("width", width);
   rect.setAttribute("height", "50");
   rect.setAttribute("class", "bar");
-  svg.appendChild(rect);
+  airlineChart.appendChild(rect);
 
-  const tooltip = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  const tooltip = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "text"
+  );
   tooltip.setAttribute("font-size", "16");
   tooltip.setAttribute("x", "0");
   // tooltip.setAttribute("text-anchor", "end");
-  tooltip.setAttribute("y", `${y+25}`);
+  tooltip.setAttribute("y", `${y + 25}`);
   tooltip.textContent = label;
-  svg.appendChild(tooltip);
-
-
+  airlineChart.appendChild(tooltip);
 }
 const airlineBars = [
-  { y: 25,  width: 250, label: "Cost" },
-  { y: 100, width: 380, label: "Speed"},
-  { y:175,  width:390,  label: "Comfort"},
-  { y:250,  width:10,   label: "Eco Friendly"}
+  { y: 25, width: 160, label: "Cost" },
+  { y: 100, width: 360, label: "Speed" },
+  { y: 175, width: 240, label: "Comfort" },
+  { y: 250, width: 120, label: "Eco Friendly" },
 ];
-for(const bar of airlineBars){
+for (const bar of airlineBars) {
   createBar(bar);
 }
