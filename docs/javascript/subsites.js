3a3c7047018b0f9d5cadda63115783853f020572
Edi Veljacic <e.veljacic@students.htl-leonding.ac.at>
2026-01-14 08:38:25 +0100

    added the graphs to the other sites

--- docs/javascript/subsites.js
+++ docs/javascript/subsites.js
@@ -1,7 +1,10 @@
 const airlineChart = document.getElementById("airlines-chart");
+const bikeChart = document.getElementById("bike-chart");
+const carChart = document.getElementById("car-chart");
+const trainChart = document.getElementById("train-chart");
+const walkChart = document.getElementById("walk-chart");
 
-createLine();
-function createLine() {
+function createLine(svg) {
   const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
   line.setAttribute("x1", "100");
   line.setAttribute("y1", "310");
@@ -9,8 +12,8 @@ function createLine() {
   line.setAttribute("y2", "310");
   line.setAttribute("stroke", "black");
   line.setAttribute("stroke-width", "2");
-  airlineChart.appendChild(line);
-
+  svg.appendChild(line);
+  
   for (let i = 0; i < 4; i++) {
     const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     line.setAttribute("x1", `${i * 133 + 100}`);
@@ -23,15 +26,15 @@ function createLine() {
   }
 }
 
-function createBar({ y, width, label }) {
+function createBar({ y, width, label, svg}) {
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   rect.setAttribute("x", `100`);
   rect.setAttribute("y", y);
   rect.setAttribute("width", width);
   rect.setAttribute("height", "50");
   rect.setAttribute("class", "bar");
-  airlineChart.appendChild(rect);
-
+  svg.appendChild(rect);
+  
   const tooltip = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "text"
@@ -41,14 +44,65 @@ function createBar({ y, width, label }) {
   // tooltip.setAttribute("text-anchor", "end");
   tooltip.setAttribute("y", `${y + 25}`);
   tooltip.textContent = label;
-  airlineChart.appendChild(tooltip);
+  svg.appendChild(tooltip);
 }
 const airlineBars = [
-  { y: 25, width: 160, label: "Cost" },
-  { y: 100, width: 360, label: "Speed" },
-  { y: 175, width: 240, label: "Comfort" },
-  { y: 250, width: 120, label: "Eco Friendly" },
+  { y: 25, width: 160, label: "Cost", svg: airlineChart},
+  { y: 100, width: 360, label: "Speed", svg: airlineChart},
+  { y: 175, width: 240, label: "Comfort", svg: airlineChart },
+  { y: 250, width: 120, label: "Eco Friendly", svg:airlineChart },
 ];
+const bikeBars = [
+  { y: 25, width: 60, label: "Cost" , svg: bikeChart},        // cheap
+  { y: 100, width: 120, label: "Speed", svg: bikeChart },
+  { y: 175, width: 220, label: "Comfort", svg: bikeChart },
+  { y: 250, width: 390, label: "Eco Friendly", svg: bikeChart },
+];
+const carBars = [
+  { y: 25, width: 300, label: "Cost", svg: carChart },       // expensive
+  { y: 100, width: 260, label: "Speed", svg: carChart },
+  { y: 175, width: 280, label: "Comfort", svg: carChart },
+  { y: 250, width: 150, label: "Eco Friendly", svg: carChart },
+];
+const trainBars = [
+  { y: 25, width: 200, label: "Cost", svg: trainChart },       // medium cost
+  { y: 100, width: 300, label: "Speed",svg: trainChart },
+  { y: 175, width: 300, label: "Comfort",svg: trainChart },
+  { y: 250, width: 320, label: "Eco Friendly",svg: trainChart },
+];
+const walkBars = [
+  { y: 25, width: 20, label: "Cost", svg:walkChart },        // almost free
+  { y: 100, width: 60, label: "Speed", svg:walkChart },
+  { y: 175, width: 200, label: "Comfort", svg:walkChart },
+  { y: 250, width: 400, label: "Eco Friendly", svg:walkChart },
+];
+
+// create all of the charts
 for (const bar of airlineBars) {
   createBar(bar);
 }
+createLine(airlineChart);
+
+
+for (const bar of bikeBars) {
+  createBar(bar);
+}
+createLine(bikeBars);
+
+
+for (const bar of carBars) {
+  createBar(bar);
+}
+createLine(carBars);
+
+
+for (const bar of trainBars) {
+  createBar(bar);
+}
+createLine(trainBars);
+
+
+for (const bar of walkBars) {
+  createBar(bar);
+}
+createLine(walkBars);
