b69d9830b0a18eed423b05460d7603ab5e11ba34
Severin Gusenleitner <Severin.gusenleitner@gmail.com>
2026-01-16 18:31:47 +0100

    formatted all documents and removed some unessecarry css rules

--- docs/javascript/subsites.js
+++ docs/javascript/subsites.js
@@ -13,7 +13,7 @@ function createLine(svg) {
   line.setAttribute("stroke", "black");
   line.setAttribute("stroke-width", "2");
   svg.appendChild(line);
-  
+
   for (let i = 0; i < 4; i++) {
     const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     line.setAttribute("x1", `${i * 133.33 + 100}`);
@@ -26,9 +26,9 @@ function createLine(svg) {
   }
 }
 
-function createBar({ y, width, label, svg}) {
+function createBar({ y, width, label, svg }) {
   const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  rect.setAttribute("x", "100");    
+  rect.setAttribute("x", "100");
   rect.setAttribute("y", y);
   rect.setAttribute("width", width);
   rect.setAttribute("height", "50");
@@ -36,12 +36,12 @@ function createBar({ y, width, label, svg}) {
   svg.appendChild(rect);
 
   const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-  text.setAttribute("x", "90");                       // left of bar
-  text.setAttribute("y", `${y + 25}`);                // vertical center of bar
-  text.setAttribute("fill", "black");                 // visible
+  text.setAttribute("x", "90"); // left of bar
+  text.setAttribute("y", `${y + 25}`); // vertical center of bar
+  text.setAttribute("fill", "black"); // visible
   text.setAttribute("font-size", "16");
-  text.setAttribute("text-anchor", "end");            // RIGHT-aligned
-  text.setAttribute("dominant-baseline", "middle");   // vertical centering
+  text.setAttribute("text-anchor", "end"); // RIGHT-aligned
+  text.setAttribute("dominant-baseline", "middle"); // vertical centering
   text.textContent = label;
   svg.appendChild(text);
 }
@@ -49,50 +49,50 @@ function createBar({ y, width, label, svg}) {
 const allBars = [
   // airplane
   [
-    { y: 25,  width: 160, label: "Cost",         svg: airlineChart },
-    { y: 100, width: 360, label: "Speed",        svg: airlineChart },
-    { y: 175, width: 240, label: "Comfort",      svg: airlineChart },
+    { y: 25, width: 160, label: "Cost", svg: airlineChart },
+    { y: 100, width: 360, label: "Speed", svg: airlineChart },
+    { y: 175, width: 240, label: "Comfort", svg: airlineChart },
     { y: 250, width: 120, label: "Eco Friendly", svg: airlineChart },
   ],
 
   // bike
   [
-    { y: 25,  width: 60,  label: "Cost",         svg: bikeChart },
-    { y: 100, width: 120, label: "Speed",        svg: bikeChart },
-    { y: 175, width: 150, label: "Comfort",      svg: bikeChart },
+    { y: 25, width: 60, label: "Cost", svg: bikeChart },
+    { y: 100, width: 120, label: "Speed", svg: bikeChart },
+    { y: 175, width: 150, label: "Comfort", svg: bikeChart },
     { y: 250, width: 390, label: "Eco Friendly", svg: bikeChart },
   ],
 
   // car
   [
-    { y: 25,  width: 300, label: "Cost",         svg: carChart },
-    { y: 100, width: 260, label: "Speed",        svg: carChart },
-    { y: 175, width: 280, label: "Comfort",      svg: carChart },
+    { y: 25, width: 300, label: "Cost", svg: carChart },
+    { y: 100, width: 260, label: "Speed", svg: carChart },
+    { y: 175, width: 280, label: "Comfort", svg: carChart },
     { y: 250, width: 150, label: "Eco Friendly", svg: carChart },
   ],
 
   // train
   [
-    { y: 25,  width: 200, label: "Cost",         svg: trainChart },
-    { y: 100, width: 300, label: "Speed",        svg: trainChart },
-    { y: 175, width: 300, label: "Comfort",      svg: trainChart },
+    { y: 25, width: 200, label: "Cost", svg: trainChart },
+    { y: 100, width: 300, label: "Speed", svg: trainChart },
+    { y: 175, width: 300, label: "Comfort", svg: trainChart },
     { y: 250, width: 320, label: "Eco Friendly", svg: trainChart },
   ],
 
   // walk
   [
-    { y: 25,  width: 20,  label: "Cost",         svg: walkChart },
-    { y: 100, width: 60,  label: "Speed",        svg: walkChart },
-    { y: 175, width: 200, label: "Comfort",      svg: walkChart },
+    { y: 25, width: 20, label: "Cost", svg: walkChart },
+    { y: 100, width: 60, label: "Speed", svg: walkChart },
+    { y: 175, width: 200, label: "Comfort", svg: walkChart },
     { y: 250, width: 400, label: "Eco Friendly", svg: walkChart },
-  ]
+  ],
 ];
-for(let diagramm = 0; diagramm<allBars.length;diagramm++){
+for (let diagramm = 0; diagramm < allBars.length; diagramm++) {
   const svg = allBars[diagramm][0].svg;
 
-    if (!svg) continue;
+  if (!svg) continue;
 
-  for(let bar = 0; bar<allBars[diagramm].length;bar++){
+  for (let bar = 0; bar < allBars[diagramm].length; bar++) {
     const currentbar = allBars[diagramm][bar];
     createBar(currentbar);
   }
